80d04d70aaca66795ad49621969db8bff12eeade
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-06-16 18:51:31 -0600

    Update

--- BZPAY_FE/src/pages/Boleteria.js
+++ BZPAY_FE/src/pages/Boleteria.js
@@ -13,8 +13,20 @@ function Boleteria() {
   const [eventsPerPage] = useState(5);
   const navigate = useNavigate();
 
+  function base64ToUint8Array(base64) {
+    const binaryString = window.atob(base64);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+
+    return bytes;
+  }
+
   const printTickets = async (item) => {
-    var id = parseInt(item.idEntrada);
+    var id = parseInt(item.id);
     console.log(id);
     const url =
       "https://localhost:7052/api/Compra/ImprimirConfirmedCompraPdf?id=" + id;
@@ -32,8 +44,23 @@ function Boleteria() {
 
     try {
       const response = await fetch(url, settings);
-      const data = await response.json();
-      console.log(response);
+      const data = await response.text();
+      console.log(data);
+      if (data != "") {
+        Swal.fire({
+          icon: "success",
+          title: "Compra procesada",
+          text: "Se ha procesado la compra correctamente",
+        });
+        const pdfData = base64ToUint8Array(data);
+        const blob = new Blob([pdfData], { type: "application/pdf" });
+        const pdfURL = URL.createObjectURL(blob);
+        const downloadLink = document.createElement("a");
+        downloadLink.href = pdfURL;
+        downloadLink.download = "file.pdf";
+        downloadLink.click();
+        navigate("/Boleteria");
+      }
       if (!response.status == 200 || !response.status == 404) {
         const message = `Un error ha ocurrido: ${response.status}`;
         throw new Error(message);
